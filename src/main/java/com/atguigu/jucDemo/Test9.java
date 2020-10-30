c1ce69c33c95c9d80ba12159b98f99a9c68df057
刘子敬 <2747754361@qq.com>
2020-10-30 17:55:57 +0800

    你好 111999

--- src/main/java/com/atguigu/jucDemo/Test9.java
+++ src/main/java/com/atguigu/jucDemo/Test9.java
@@ -54,7 +54,7 @@ public class Test9 {
 
 
         TimeUnit.SECONDS.sleep(2);
-        System.out.println("111");
+        System.out.println("111999");
 
         for (int i = 1; i <=5 ; i++) {
             String num = String.valueOf(i);
